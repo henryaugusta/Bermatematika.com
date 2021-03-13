4269abcfe719742677891b022928747771d39fef
henryaugusta <henryaugusta4@gmail.com>
2021-03-13 19:51:26 +0700

    finishing main menu layout

--- app/src/main/java/com/feylabs/bermatematika/MainActivity.kt
+++ app/src/main/java/com/feylabs/bermatematika/MainActivity.kt
@@ -1,11 +1,53 @@
 package com.feylabs.bermatematika
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.feylabs.bermatematika.databinding.ActivityMainBinding
+import kotlinx.coroutines.*
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.concurrent.fixedRateTimer
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var vbinding: ActivityMainBinding
+    lateinit var d : Date
+    lateinit var meTime : String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        vbinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(vbinding.root)
+
+        val sdf = SimpleDateFormat("kk:mm:ss")
+        d = Date()
+        meTime = sdf.format(d)
+
+        fixedRateTimer("timer",false,0,1000){
+            this@MainActivity.runOnUiThread {
+                d = Date()
+                meTime = sdf.format(d)
+                vbinding.textB.text = meTime
+            }
+        }
+
+
+        val test2 = DateFormat.getDateInstance(DateFormat.DATE_FIELD).format(Date())
+        val test4 = DateFormat.getDateInstance(DateFormat.ERA_FIELD).format(Date())
+        Log.d("test1",meTime.toString())
+        Log.d("test2",test2.toString())
+        Log.d("test4",test4.toString())
+
+        vbinding.textU.text = test4
+
+
+        vbinding.btnStart.setOnClickListener {
+            startActivity(Intent(this, MenuActivity::class.java))
+        }
     }
+
 }
