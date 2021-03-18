c4c3c5d2e1d37efb844e6951bb54d06c4ed419b3
henryaugusta <henryaugusta4@gmail.com>
2021-03-18 23:10:49 +0700

    import FAN and prepare Model

--- app/src/main/java/com/feylabs/bermatematika/model/notasi/NotasiModel.kt
+++ app/src/main/java/com/feylabs/bermatematika/model/notasi/NotasiModel.kt
@@ -5,14 +5,16 @@ import com.feylabs.bermatematika.model.class7.Class7Model
 class NotasiModel {
     lateinit var name: String
     lateinit var read: String
-    lateinit var expl: String
+    lateinit var desc: String
+    lateinit var audioPath: String
 
     lateinit var data : MutableList<NotasiModel>
 
-    constructor(name : String,read : String,explanation : String){
+    constructor(name : String,read : String,desc : String , audioPath : String){
         this.name = name
         this.read = read
-        this.expl = explanation
+        this.desc = desc
+        this.audioPath = audioPath
     }
 
     fun setFormulaData(newData : MutableList<NotasiModel>){
